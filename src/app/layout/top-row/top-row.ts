34ff6b1aa3c28a7a3200a7ff7728f4c0ea526dda
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2026-06-14 21:14:51 +0200

    Hide dashboard widgets in focus mode

--- src/app/layout/top-row/top-row.ts
+++ src/app/layout/top-row/top-row.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MoveWidget } from '../../features/move/move-widget';
 import { FocusWidget } from '../../features/focus/focus-widget/focus-widget';
 import { WeatherWidget } from '../../features/weather/weather-widget/weather-widget';
+import { FocusService } from '../../features/focus/focus-service';
 
 @Component({
   selector: 'app-top-row',
   templateUrl: './top-row.html',
   imports: [MoveWidget, FocusWidget, WeatherWidget],
 })
-export class TopRow {}
+export class TopRow {
+  private readonly focusService = inject(FocusService);
+
+  readonly focusMode = computed(() => this.focusService.inFocusMode());
+}
